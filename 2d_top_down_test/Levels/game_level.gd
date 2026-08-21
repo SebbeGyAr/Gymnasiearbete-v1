3ed5fe8f6984d45ffc7dacc7a52f943043cb4664
Sebbe <sebastian.lundback@edu.vellinge.se>
2026-08-21 12:04:42 +0200

    Update game_level.gd

--- 2d_top_down_test/Levels/game_level.gd
+++ 2d_top_down_test/Levels/game_level.gd
@@ -22,23 +22,34 @@ func _physics_process(delta: float) -> void:
 	)
 	
 	#print($PlayerCharacter.get_screen_transform()[2])
-	
+	var restoreMult = 1
 	var target_distance = 75
 	var camera_speed = 50 * delta
 	
 	var camera = $PlayerCharacter/Camera2D
-	
+	#print(DisplayServer.window_get_size())
 	if inputDirection.x > 0: 
-		camera.offset.x = min(camera.offset.x + camera_speed, (16/9) * target_distance)
+		if $PlayerCharacter.get_screen_transform()[2].x > DisplayServer.window_get_size().x / 2 - 1:
+			restoreMult = 2
+		else: restoreMult = 1
+		camera.offset.x = min(camera.offset.x + camera_speed * restoreMult, (16/9) * target_distance)
 		
 	elif inputDirection.x < 0: 
-		
-		camera.offset.x = max(camera.offset.x - camera_speed, -(16/9) * target_distance)
+		if $PlayerCharacter.get_screen_transform()[2].x < DisplayServer.window_get_size().x / 2 - 1:
+			restoreMult = 2
+		else: restoreMult = 1
+		camera.offset.x = max(camera.offset.x - camera_speed * restoreMult, -(16/9) * target_distance)
 		
 	if inputDirection.y > 0:
-		camera.offset.y = min(camera.offset.y + camera_speed, target_distance)
+		if $PlayerCharacter.get_screen_transform()[2].y > DisplayServer.window_get_size().y / 2 - 1:
+			restoreMult = 2
+		else: restoreMult = 1
+		camera.offset.y = min(camera.offset.y + camera_speed * restoreMult, target_distance)
 		
 	elif inputDirection.y < 0:
+		if $PlayerCharacter.get_screen_transform()[2].y < DisplayServer.window_get_size().y / 2 - 1:
+			restoreMult = 2
+		else: restoreMult = 1
 		camera.offset.y = max(camera.offset.y - camera_speed, -target_distance)
 	
 	if inputDirection == Vector2.ZERO:

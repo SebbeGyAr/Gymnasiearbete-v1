2ef8c75c7dc4fbbe6d62bf9c49c26603dcc041a8
Sebbe <sebastian.lundback@edu.vellinge.se>
2026-09-10 08:07:11 +0200

    ingenting att notera

--- Gymnasiearbete spel v1/UI/Overlay/flashlight.gd
+++ Gymnasiearbete spel v1/UI/Overlay/flashlight.gd
@@ -8,17 +8,16 @@ const BATTERY_DRAIN_SPEED = 0.005
 func _ready() -> void:
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var t = 1.0 - exp(-SMOOTH_SPEED * delta)
 	var direction = (get_global_mouse_position() - global_position)
 	rotation = lerp_angle(rotation, direction.angle(), t)
-	
+
 	if not (GlobalVariables.hasFlashlight and GlobalVariables.flashlightEnabled) or GlobalVariables.flashlightBattery <= 0:
 		flashlight.enabled = false
 	else:
 		flashlight.enabled = true
-	
+
 	if GlobalVariables.flashlightBattery < 10 and GlobalVariables.flashlightEnabled: 
 		flashlight.energy = 0
 		await get_tree().create_timer(randf()).timeout

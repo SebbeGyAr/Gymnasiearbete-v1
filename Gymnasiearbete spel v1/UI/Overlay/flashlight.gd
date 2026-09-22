9335a5c8f99d5613f5490f3dbf7b908b18e41b0a
Sebbe <sebastian.lundback@edu.vellinge.se>
2026-09-22 17:17:01 +0200

    Flashlightändringar, gammal kod bortkommenterad så kan plockas tillbaka när som

--- Gymnasiearbete spel v1/UI/Overlay/flashlight.gd
+++ Gymnasiearbete spel v1/UI/Overlay/flashlight.gd
@@ -1,7 +1,7 @@
-extends Node2D
+"extends Node2D
 
 # HUR SMOOTH SJÄLVA LJUSSTRÅLEN RÖR SIG
-const SMOOTH_SPEED = 5
+const SMOOTH_SPEED = 10
 
 # HUR MYCKET BATTERI SOM FÖRSVINNER VARJE DELTA
 const BATTERY_DRAIN_SPEED = 0.001
@@ -9,6 +9,7 @@ const BATTERY_DRAIN_SPEED = 0.001
 var isFlickering = false
 var isRandomFlickering = false
 @onready var flashlight = $Flashlight
+@onready var target = $FlashlightTarget
 
 func _ready() -> void:
 	random_flashlight_flicker_loop()
@@ -52,6 +53,75 @@ func random_flashlight_flicker_loop():
 	# STÄNGER AV SIG SJÄLV OM DET SNABBA FLICKRANDET TAR ÖVER
 	while GlobalVariables.flashlightBattery > 10:
 		# VÄNTAR EN RANDOM PERIOD MELLAN 1 OCH 4 SEK OCH FLICKERAR, SAMMA SAFEGUARDS SOM VANLIGA flashlight_flicker()
+		await get_tree().create_timer(randf_range(1.0, 4.0)).timeout
+		isRandomFlickering = true
+		flashlight.energy = 0
+		
+		await get_tree().create_timer(randf_range(0.0, 0.1)).timeout
+		isRandomFlickering = false
+		flashlight.energy = 1
+"
+extends Node2D
+
+# HUR SMOOTH SJÄLVA LJUSSTRÅLEN RÖR SIG
+const SMOOTH_SPEED = 10
+
+# HUR MYCKET BATTERI SOM FÖRSVINNER VARJE DELTA
+const BATTERY_DRAIN_SPEED = 0.001
+const TARGET_BASE_POSITION = Vector2(25, 0)
+
+var isFlickering = false
+var isRandomFlickering = false
+var lastInputDirection = Vector2.RIGHT
+@onready var flashlight = $Flashlight
+@onready var target = $FlashlightTarget
+
+
+func _ready() -> void:
+	random_flashlight_flicker_loop()
+
+func _process(delta: float) -> void:
+	var t = 1.0 - exp(-SMOOTH_SPEED * delta)
+	var inputDirection = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"), 
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	
+	if inputDirection != Vector2.ZERO:
+		lastInputDirection = inputDirection.normalized()
+	
+	# TAR SENASTE LOKALA POSITIONEN
+	target.position = TARGET_BASE_POSITION.rotated(lastInputDirection.angle())
+	
+	# target.position ÄR SAMMA SOM RIKTNINGSVEKTORN EFTERSOM ÄNDRINGEN ÄR LOKAL
+	rotation = lerp_angle(rotation, target.position.angle(), t)
+	
+	if not (GlobalVariables.hasFlashlight and GlobalVariables.flashlightEnabled) or GlobalVariables.flashlightBattery <= 0:
+		flashlight.enabled = false
+	else:
+		flashlight.enabled = true
+	
+	if GlobalVariables.flashlightBattery < 10 and GlobalVariables.flashlightEnabled: 
+		flashlight_flicker()
+		
+	if GlobalVariables.flashlightEnabled and GlobalVariables.hasFlashlight and GlobalVariables.flashlightBattery > 0: 
+		GlobalVariables.flashlightBattery -= BATTERY_DRAIN_SPEED
+
+func flashlight_flicker():
+	if isFlickering:
+		return
+	isFlickering = true
+	
+	flashlight.energy = 0
+	await get_tree().create_timer(randf_range(0, 0.1)).timeout
+	
+	flashlight.energy = 1
+	await get_tree().create_timer(randf_range(0, 0.5)).timeout
+	
+	isFlickering = false
+
+func random_flashlight_flicker_loop():
+	while GlobalVariables.flashlightBattery > 10:
 		await get_tree().create_timer(randf_range(1.0, 4.0)).timeout
 		isRandomFlickering = true
 		flashlight.energy = 0
